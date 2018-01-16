b6d454e67b0ee7189f4d1951157b30c5a7172da3
yFernandess <fernandes.yuri@hotmail.com>
2018-01-16 10:15:45 -0200

    Alterado layout da pagina home e adicionado a pagina contato

--- simplemooc/code/simplemooc/simplemooc/urls.py
+++ simplemooc/code/simplemooc/simplemooc/urls.py
@@ -1,3 +1,5 @@
+# coding: utf-8
+
 from django.conf.urls import patterns, include, url
 
 from django.contrib import admin
@@ -5,5 +7,6 @@ admin.autodiscover()
 
 urlpatterns = patterns('',
     url(r'^$', 'simplemooc.core.views.home', name='home'),
+    url(r'^contato/$', 'simplemooc.core.views.contact', name='contact'),
     url(r'^admin/', include(admin.site.urls)),
 )
